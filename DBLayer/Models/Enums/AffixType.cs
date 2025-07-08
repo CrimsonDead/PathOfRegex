3c7573a4676f99485cfddca031532dc489e4d0be
Kirill Dubanevich <k.dubanevich@ic-service.net>
2025-07-08 18:37:19 +0300

    Initiating db

---
+++ DBLayer/Models/Enums/AffixType.cs
@@ -0,0 +1,8 @@
+﻿namespace PathOfRegexDB.Models.Enums
+{
+    public enum AffixType
+    {
+        Suffix = 0,
+        Prefix = 1
+    }
+}
---
+++ DBLayer/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PathOfRegexDB.Models
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, NotNull]
+        public required string Name { get; set; }
+        
+        [Required, NotNull]
+        [ForeignKey("ItemTypeMaster")]
+        public required ItemTypeMaster ItemType{ get; set; }
+
+        public string? Alias { get; set; }
+    }
+}
